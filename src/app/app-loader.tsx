640195c38570f6b270578766c527ea7ca4930977
KRECER <sudenkoweb@gmail.com>
2024-02-25 11:21:04 +0200

    feat(app-initialization): preload task data and add tasks page route

--- src/app/app-loader.tsx
+++ src/app/app-loader.tsx
@@ -1,5 +1,6 @@
 import { useBoards } from "@/entities/board";
 import { useSession } from "@/entities/session";
+import { useTasksStore } from "@/entities/task";
 import { useUsers } from "@/entities/user";
 import { UiPageSpinner } from "@/shared/ui/ui-page-spinner";
 import { ReactNode, useEffect, useState } from "react";
@@ -8,15 +9,16 @@ export function AppLoader({ children }: { children?: ReactNode }) {
   const loadUsers = useUsers((s) => s.loadUsers);
   const loadSession = useSession((s) => s.loadSession);
   const loadBoards = useBoards((s) => s.loadBoards);
+  const loadTasks = useTasksStore((s) => s.loadTasks);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     setIsLoading(true);
 
-    Promise.all([loadSession(), loadUsers(), loadBoards()]).finally(() => {
+    Promise.all([loadSession(), loadUsers(), loadBoards(), loadTasks()]).finally(() => {
       setIsLoading(false);
     });
-  }, [loadSession, loadUsers, loadBoards]);
+  }, [loadSession, loadUsers, loadBoards, loadTasks]);
 
   if (isLoading) {
     return <UiPageSpinner />;
